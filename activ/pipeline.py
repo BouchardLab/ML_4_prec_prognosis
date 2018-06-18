c2c8699f36d0ea87f36a75bab699a8e16d2964ba
Ahyeon Hwang <ahyeon96@berkeley.edu>
2018-06-18 15:23:05 -0700

    moving files

--- activ/pipeline.py
+++ activ/pipeline.py
@@ -43,40 +43,6 @@ def build_predictive_model(biomarkers, outcomes, bio_method, out_method, sl_meth
     score = sl_method(biomarkers_dec, outcomes_dec, sl_method_obj)
     return score
 
-if __name__ == '__main__':
-
-    # biomarker_dec: NMF, ICA, DL (dictionary learning)
-    # outcome_dec: PCA, FA (factor analysis), MDS (multidim scaling), UMAP
-    
-    mds = MDS()
-    lr = LogisticRegression()
-        
-    # biomarkers = [0,1,2,3,4,5]
-    # outcomes = [1,2,3,4,5,6]
-    # mnist = datasets.load_digits()
-    # mnist_X = mnist.data
-    # mnist_y = mnist.target
-
-    # mnist_X = biomarker_dec(mnist_X, pca)
-    # mnist_y = outcome_dec(mnist_y, pca)
-
-    # X_train, X_test, y_train, y_test = train_test_split(mnist_X, mnist_y, test_size = 0.2)
-    
-    # accuracy = sl_method(X_train, X_test, y_train, y_test, lr, score_func = accuracy_score)
-    # print('accuracy: {}'.format(accuracy))
-
-    data = '/Users/ahyeon/Desktop/TBIanalysis/data/data.h5'
-    f = h5py.File(data, 'r')
-    data_matrix_subset_biomarker = f['data_matrix_subset_biomarker']
-    data_matrix_subset_outcome = f['data_matrix_subset_outcome']
-    feature_name_subset_biomarker = f['feature_name_subset_biomarker']
-    feature_name_subset_outcome = f['feature_name_subset_outcome']
-    patient_id = f['patient_id']
-    for n in range(1,11):
-        pca = PCA(n_components = n)
-        fa = FactorAnalysis(n_components = n)
-        method = best_decomp_method(data_matrix_subset_biomarker, pca, fa)
-        print(method)
 
   
     
