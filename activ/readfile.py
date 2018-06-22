2f0e7cdb047f0539f15657b8a3f680253f2409e5
Ahyeon Hwang <ahyeon96@berkeley.edu>
2018-06-22 10:45:54 -0700

    changed names

--- activ/readfile.py
+++ activ/readfile.py
@@ -7,8 +7,8 @@ class ReadFile(object):
         self.filename = filename
 
         with open(self.filename) as f:
-            self.data_matrix_subset_biomarker = f['data_matrix_subset_biomarker'][:]
-            self.data_matrix_subset_outcome = f['data_matrix_subset_outcome'][:]
-            self.feature_name_subset_biomarker = f['feature_name_subset_biomarker'][:]
-            self.feature_name_subset_outcome = f['feature_name_subset_outcome'][:]
-            self.patient_id = f['patient_id'][:]
+            self.data_bm = f['data_matrix_subset_biomarker'][:]
+            self.data_oc = f['data_matrix_subset_outcome'][:]
+            self.feature_bm = f['feature_name_subset_biomarker'][:]
+            self.feature_oc = f['feature_name_subset_outcome'][:]
+            self.id = f['patient_id'][:]
