e871d416326a1ad11b85ac227ad3a6397bdc686a
Ahyeon <ahyeon@animalcrackers.dhcp.lbl.gov>
2018-07-30 22:33:32 -0700

    updated script

--- bin/run_cluster_size_pipeline.py
+++ bin/run_cluster_size_pipeline.py
@@ -5,6 +5,8 @@ import h5py
 from umap import UMAP
 from sklearn.ensemble import RandomForestClassifier as RFC
 from sklearn.model_selection import train_test_split, cross_val_score
+import deepdish as dd
+import warnings
 
 def read_h5(filename):
     f = h5py.File(filename, 'r')
@@ -22,31 +24,33 @@ def umap_pipeline(iteration, test_data, dataname, n_jobs=1):
     for x in range(iteration):
         embeddings = []
         trees = []
+        print ("--------- iteration %s ---------" % x)
         for ii,k in enumerate(umap_dims): # umap dimension
-            embedding = run_umap(test_data, k, n_neighbors=10, min_dist=0.000, random_state=1)
+            embedding = run_umap(test_data, k, n_neighbors=10, min_dist=0.000)
             Z = hierarchy.linkage(embedding, 'ward', optimal_ordering=True)
             trees.append(Z)
             for jj,c in enumerate(clusters): # cluster
+                print('umap dimensions: %s, num clusters: %s' % (k, c))
                 labels = hierarchy.cut_tree(Z, n_clusters=c).reshape(237)
-                score[x,ii,jj] = cross_val_score(RFC(100, n_jobs=n_jobs), test_data, labels, cv=10)
+                warnings.filterwarnings("ignore")
+                score[x,ii,jj] = cross_val_score(RFC(100, n_jobs=n_jobs), test_data, labels, cv=5)
                 norm_score[x,ii,jj] = cross_val_score(RFC(100, n_jobs=n_jobs), test_data, np.random.permutation(labels), cv=10)
         embdict[x] = embeddings
         treedict[x] = trees
-        print(x)
-#     fname = '/Users/ahyeon/Desktop/TBIanalysis/umap_{}_results.h5'.format(dataname)
-    fname = '/home/ahyeon/data/umap_{}_results.h5'.format(dataname)
+    fname = '/home/ahyeon/data/cv_results_{}_{}.h5'.format(dataname,iteration)
     with h5py.File(fname, 'w') as f:
         f.create_dataset('score', data=score)
         f.create_dataset('norm_score', data=norm_score)
-#     dd.io.save('/Users/ahyeon/Desktop/TBIanalysis/{}_embeddings.h5'.format(dataname), embdict)
-#     dd.io.save('/Users/ahyeon/Desktop/TBIanalysis/{}_trees.h5'.format(dataname), treedict)
-    dd.io.save('/home/ahyeon/data/{}_embeddings.h5'.format(dataname), embdict)
-    dd.io.save('/home/ahyeon/data/{}_trees.h5'.format(dataname), treedict)
+    dd.io.save('/home/ahyeon/data/embeddings_{}_{}.h5'.format(dataname,iteration), embdict)
+    dd.io.save('/home/ahyeon/data/trees_{}_{}.h5'.format(dataname,iteration), treedict)
     return score, norm_score, embdict, treedict
 
 oc_W1 = '/home/ahyeon/data/data_matrix_subset_outcome.h5'
-
+bm_W1 = '/home/ahyeon/data/data_matrix_subset_biomarker.h5'
 oc_w1 = read_h5(oc_W1)
+bm_w1 = read_h5(bm_W1)
 oc_w1, indices = filter_outliers(oc_w1,10)
+bm_w1, indices = filter_outliers(bm_w1,10)
 
-score, norm_score, emb, tree = umap_pipeline(50, oc_w1, 'oc_w1', 8)
+umap_pipeline(1, bm_w1, 'bm_w1', 8)
+umap_pipeline(50, bm_w1, 'bm_w1', 8)
