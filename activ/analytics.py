4592900358a3341a0f34d8ee55e0a8cae19caa2a
Andrew Tritt <ajtritt@lbl.gov>
2018-07-31 18:58:36 -0700

    added function for easily generating a 2D heatmap

---
+++ activ/analytics.py
@@ -0,0 +1,60 @@
+def heatmap(data, row_labels, col_labels, ax=None,
+            cbar_kw={}, cbarlabel="", xlab=None, ylab=None,
+            title=None, **kwargs):
+    """
+    Create a heatmap from a numpy array and two lists of labels.
+
+    Arguments:
+        data       : A 2D numpy array of shape (N,M)
+        row_labels : A list or array of length N with the labels
+                     for the rows
+        col_labels : A list or array of length M with the labels
+                     for the columns
+    Optional arguments:
+        xlab       : the label for the x-axis
+        ylab       : the label for the y-axis
+        title      : the title for the heatmap
+        ax         : A matplotlib.axes.Axes instance to which the heatmap
+                     is plotted. If not provided, use current axes or
+                     create a new one.
+        cbar_kw    : A dictionary with arguments to
+                     :meth:`matplotlib.Figure.colorbar`.
+        cbarlabel  : The label for the colorbar
+    All other arguments are directly passed on to the imshow call.
+    """
+
+    if not ax:
+        ax = plt.gca()
+
+    # Plot the heatmap
+    im = ax.imshow(data, **kwargs)
+
+    # Create colorbar
+    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
+    cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
+
+    # We want to show all ticks...
+    ax.set_xticks(np.arange(data.shape[1]))
+    ax.set_yticks(np.arange(data.shape[0]))
+    # ... and label them with the respective list entries.
+    ax.set_xticklabels(col_labels)
+
+    ax.set_yticklabels(row_labels)
+    ax.set_xlabel(None)
+    if xlab is not None:
+        ax.set_xlabel(xlab)
+    if ylab is not None:
+        ax.set_ylabel(ylab)
+    if title is not None:
+        ax.set_title(title)
+
+    # Let the horizontal axes labeling appear on top.
+    #ax.tick_params(top=True, bottom=False,
+    #               labeltop=True, labelbottom=False)
+
+    # Rotate the tick labels and set their alignment.
+    #plt.setp(ax.get_xticklabels(), rotation=-30, ha="right",
+    #         rotation_mode="anchor")
+
+
+    return im, cbar
