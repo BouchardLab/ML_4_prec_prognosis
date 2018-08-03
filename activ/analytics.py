f4653546bebf77abbaddad1d36263d12a39c5d99
Ahyeon Hwang <ahyeon96@berkeley.edu>
2018-08-03 11:50:38 -0700

    oc factors heatmap function

--- activ/analytics.py
+++ activ/analytics.py
@@ -1,5 +1,6 @@
 import numpy as np
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib.pyplot as plt
 
 def heatmap(data, row_labels, col_labels, ax=None,
             cbar_kw={}, cbarlabel="", xlab=None, ylab=None,
@@ -25,7 +26,6 @@ def heatmap(data, row_labels, col_labels, ax=None,
         cbarlabel  : The label for the colorbar
     All other arguments are directly passed on to the imshow call.
     """
-    import matplotlib.pyplot as plt
 
     if not ax:
         ax = plt.gca()
@@ -47,7 +47,73 @@ def heatmap(data, row_labels, col_labels, ax=None,
     ax.set_xticks(np.arange(data.shape[1]))
     ax.set_yticks(np.arange(data.shape[0]))
     # ... and label them with the respective list entries.
-    ax.set_xticklabels(col_labels, rotation=45)
+    ax.set_xticklabels(col_labels)
+
+    ax.set_yticklabels(row_labels)
+    ax.set_xlabel(None)
+    if xlab is not None:
+        ax.set_xlabel(xlab)
+    if ylab is not None:
+        ax.set_ylabel(ylab)
+    if title is not None:
+        ax.set_title(title)
+
+    # Let the horizontal axes labeling appear on top.
+    #ax.tick_params(top=True, bottom=False,
+    #               labeltop=True, labelbottom=False)
+
+    # Rotate the tick labels and set their alignment.
+    #plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+    #        rotation_mode="anchor")
+
+    return im, cbar
+
+def outcome_factors_heatmap(data, row_labels, col_labels, ax=None,
+            cbar_kw={}, cbarlabel="", xlab=None, ylab=None,
+            title=None, **kwargs):
+    """
+    Create a heatmap from outcome factors.
+
+    Arguments:
+        data       : A 2D numpy array of shape (N,M)
+        row_labels : A list or array of length N with the outcome
+                     factors for the rows
+        col_labels : A list or array of length M with the outcome
+                     labels for the columns
+    Optional arguments:
+        xlab       : the label for the x-axis
+        ylab       : the label for the y-axis
+        title      : the title for the heatmap
+        ax         : A matplotlib.axes.Axes instance to which the heatmap
+                     is plotted. If not provided, use current axes or
+                     create a new one.
+        cbar_kw    : A dictionary with arguments to
+                     :meth:`matplotlib.Figure.colorbar`.
+        cbarlabel  : The label for the colorbar
+    All other arguments are directly passed on to the imshow call.
+    """
+
+    if not ax:
+        ax = plt.gca()
+
+    kwargs.setdefault('cmap', 'binary')
+    # Plot the heatmap
+    im = ax.imshow(data, **kwargs)
+
+    # create an axes on the right side of ax. The width of cax will be 5%
+    # of ax and the padding between cax and ax will be fixed at 0.05 inch.
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="2%", pad=0.1)
+
+    # Create colorbar
+    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw, cax=cax)
+    cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
+
+    # We want to show all ticks...
+    ax.set_xticks(np.arange(data.shape[1]))
+    ax.set_yticks(np.arange(data.shape[0]))
+    # ... and label them with the respective list entries.
+    ax.set_xticklabels(col_labels)
 
     ax.set_yticklabels(row_labels)
     ax.set_xlabel(None)
@@ -58,12 +124,9 @@ def heatmap(data, row_labels, col_labels, ax=None,
     if title is not None:
         ax.set_title(title, fontsize=48)
 
-    # Let the horizontal axes labeling appear on top.
-    #ax.tick_params(top=True, bottom=False,
-    #               labeltop=True, labelbottom=False)
-
     # Rotate the tick labels and set their alignment.
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right")
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+            rotation_mode="anchor")
 
 
-    return im, cbar
+
