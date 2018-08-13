f3683dcb8ac022e4e137f5f7e10d043ce38dbf52
Andrew Tritt <ajtritt@lbl.gov>
2018-08-13 14:02:45 -0700

    add ability to pass in precomputed embeddings to cluster pipeline

--- bin/run_cluster_size_pipeline.py
+++ bin/run_cluster_size_pipeline.py
@@ -3,13 +3,9 @@ import sys
 from datetime import datetime
 import argparse
 import logging
-from mpi4py import MPI
-
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
 
 from activ.readfile import TrackTBIFile
-from activ.clustering import umap_cluster_sweep
+from activ.clustering import umap_cluster_sweep, UmapClusteringResults
 from activ import load_data
 
 def int_list(string):
@@ -27,11 +23,13 @@ parser.add_argument("-d", "--data", type=str, help="the Track TBI dataset file. 
 parser.add_argument("-p", "--pdata", type=str, help="the Track TBI dataset file to use for predictions. use activ.load_data(uoinmf=True) by default", default=None)
 parser.add_argument("-s", "--self_predict", help="predict clusters with dataset used to form clusters", action='store_true', default=False)
 parser.add_argument("-b", "--biomarkers", help="form clusters using biomarkers. use outcomes by default", action='store_true', default=False)
+parser.add_argument("-m", "--metric", help="the distance metric to compute clusters with", default="mahalanobis")
 parser.add_argument("-i", "--iters", type=int, help="the number of iterations to run", default=50)
 parser.add_argument("-u", "--umap_dims", type=int_list, help="a comma-separated list of the UMAP dimensions",
                     default=list(range(2, 11)))
 parser.add_argument("-c", "--cluster_size", type=int_list, help="a comma-separated list of the cluster sizes",
                     default=list(range(2, 15)))
+parser.add_argument("-e", "--embeddings", type=str, help="the path to the file to use embeddings from", default=None)
 parser.add_argument("-l", "--log", type=str, help="the file to write log messages to [stderr]", default=None)
 parser.add_argument("-f", "--force", help="force rerunning i.e. overwrite output_h5", action='store_true', default=False)
 parser.add_argument("-q", "--quiet", help="make log messages quiet", action='store_true', default=False)
@@ -43,6 +41,18 @@ if len(sys.argv) == 1:
 pargs = parser.parse_args()
 
 
+rank = 0
+nranks = 1
+comm = None
+try:
+    from mpi4py import MPI
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+except ImportError:
+    rank = 0
+    nranks = 1
+    comm = None
+
 
 args = list()
 kwargs = dict()
@@ -51,7 +61,8 @@ kwargs = dict()
 logger_name = 'umap_cluster_sweep'
 handler = None
 logfmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-if comm.Get_size() > 1:
+
+if nranks > 1:
     from activ.mpitools import MPILogHandler
     logger_name = logger_name + "-%d" % rank
     if pargs.log is None:
@@ -84,6 +95,8 @@ if pargs.pdata is None:
 else:
     pdata = TrackTBIFile(pargs.pdata)
 
+hdf5_to_close = list()
+
 def log1(msg):
     if rank == 0:
         logger.info(msg)
@@ -110,7 +123,19 @@ else:
         log1('predicting cluster labels with biomarkers from %s' % pdata.filename)
         kwargs['predict_data'] = pdata.biomarkers
 
-args.append(pargs.umap_dims)
+if pargs.embeddings is not None:
+    fkwargs = {'mode': 'r'}
+    if comm is not None:
+        fkwargs['driver'] = 'mpio'
+        fkwargs['comm'] = comm
+    f = h5py.File(pargs.embeddings, **fkwargs)
+    hdf5_to_close.append(f)
+    kwargs['precomputed_embeddings'] = f[UmapClusteringResults.path.embeddings]
+    args.append(f[UmapClusteringResults.path.umap_dimensions][:])
+    log("Using dimensions and embeddings from %s" % pargs.embeddings)
+else:
+    args.append(pargs.umap_dims)
+
 args.append(pargs.cluster_size)
 
 path = pargs.output_h5
@@ -118,9 +143,13 @@ if os.path.exists(path) and not pargs.force:
     sys.stderr.write("%s already exists. cautiously exiting. use -f to override\n" % path)
     sys.exit(255)
 kwargs['h5group'] = path
+
 start = datetime.now()
 log1("Begin UMAP pipeline at %s" % start.strftime('%Y-%m-%d %H:%M:%S'))
 res = umap_cluster_sweep(*args, **kwargs)
 end = datetime.now()
 log1("End UMAP pipeline at %s" % end.strftime('%Y-%m-%d %H:%M:%S'))
 log1("Time elapsed: %s" % str(end-start))
+
+for f in hdf5_to_close:
+    f.close()
