b62cbf19dea3203d2a056955bbafe8f6abd10248
Ahyeon Hwang <ahyeon96@berkeley.edu>
2018-08-17 12:33:56 -0700

    more plots

--- bin/summarize_umap_clustering_results.py
+++ bin/summarize_umap_clustering_results.py
@@ -1,17 +1,24 @@
-%matplotlib inline
-import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
 import h5py
 import numpy as np
 from activ.analytics import heatmap
 from activ.clustering import UmapClusteringResults
 from os.path import dirname, join
 from matplotlib.backends.backend_pdf import PdfPages
+import matplotlib.pyplot as plt
+import argparse
 
+parser = argparse.ArgumentParser(description='Create summary pdf of umap clustering results.')
+parser.add_argument('path', type=str, help='path to h5 file')
+parser.add_argument('filename', type=str, help='name of file')
+args = parser.parse_args()
 
-path = "../results/mahalanobis_output/umap_cluster_uoinmf-mahalanobis.h5"
-pdf = PdfPages(join(dirname(path), 'umap_clustering_sweep_mahalanobis_summary.pdf'))
+#path = "../results/mahalanobis_output/umap_cluster_uoinmf-mahalanobis.h5"
+#pdf = PdfPages(join(dirname(path), 'umap_clustering_sweep_mahalanobis_summary.pdf'))
 
-results = UmapClusteringResults(path)
+pdf = PdfPages(join(dirname(args.path), args.filename))
+results = UmapClusteringResults(args.path)
 plt.figure(figsize=(15, 5))
 ret = results.make_heatmap()
 pdf.savefig()
