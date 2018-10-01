3d98e66e7642a156339d1655abf1d281c58bcc06
Ahyeon Hwang <ahyeon96@berkeley.edu>
2018-10-01 16:18:37 -0700

    fixed mpi

--- bin/optimize_k.py
+++ bin/optimize_k.py
@@ -18,31 +18,24 @@ comm = MPI.COMM_WORLD
 name = MPI.Get_processor_name()
 rank = comm.Get_rank()
 size = comm.Get_size()
-root_ = 0
-TAG_BLOCK_IDX = 1
 
 data = load_data()
 data_oc = data_normalization(data.outcomes, 'positive')
-train_oc, test_oc = train_test_split(data_oc)
 
 def optimize_k_error(data, mpicomm=None):
     eps = np.arange(0.1,1.1,0.1)
     minsamples = np.arange(5,55,5)
+    n_splits = 10
     kf = KFold(n_splits=10)
+    indices = range(rank, nsplits, size)
     error_mat = np.zeros((10,10,10))
-    for k, (train_index, test_index) in enumerate(kf.split(data_oc)):
+
+    for k, (train_index, test_index) in zip(indices, kf.split(data_oc)):
         train, test = data_oc[train_index], data_oc[test_index]
         train_oc = train
         test_oc = test
 
         for ii,e in enumerate(eps):
-            if rank == root_:
-                for worker_idx in (1+np.arange(size-1)):
-                    comm.send(ii,
-                    dest = worker_idx,
-                    tag = TAG_BLOCK_IDX)
-            else:
-                local_block_idx = comm.recv(source=root_, tag=TAG_BLOCK_IDX)
 
             for jj,m in enumerate(minsamples):
                 db = DBSCAN(eps=e, min_samples=m)
@@ -51,12 +44,13 @@ def optimize_k_error(data, mpicomm=None):
                 H = decomp.components_
                 W = decomp.transform(test_oc, reconstruction_err=True)
                 error = np.linalg.norm(test_oc-W@H)
-                error_mat[ii,jj,k] = errorerror_mat = optimize_k_error(data_oc)
+                error_mat[ii,jj,k] = error_mat = optimize_k_error(data_oc)
     return error_mat
 
 error_mat = optimize_k_error(data_oc)
-h5f = h5py.File('optimize_k_mat.h5', 'w')
-h5f.create_dataset('mat', data=error_mat)
 
+h5f = h5py.File('optimize_k_mat.h5', 'w', driver="mpio", comm=MPI.COMM_WORLD)
+h5f.create_dataset('mat', data=error_mat)
+h5f.close()
 
 plt.matshow(error_mat, cmap='gray_r')
