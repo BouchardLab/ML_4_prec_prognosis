4fb659cada45aa2cd62e000898bf1ca53300ba1c
Ahyeon Hwang <ahyeon96@berkeley.edu>
2018-10-12 12:37:42 -0700

    added argparse to optimize_k script

--- bin/optimize_k.py
+++ bin/optimize_k.py
@@ -4,15 +4,20 @@ from activ import load_data, data_normalization
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy
-from activ import load_data
 from PyUoI.UoINMF import UoINMF
 from sklearn.model_selection import train_test_split
 from sklearn.cluster import DBSCAN
-from activ.data_normalization import data_normalization
 from scipy.spatial.distance import pdist
 from sklearn.model_selection import KFold
 from mpi4py import MPI
 import h5py
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('eps', type=list, help='a list of eps values', default=list(range(0.3,0.6,0.1)))
+parser.add_argument('minsamples', type=int_list, help='a list of minsample values', default=list(range(5,55,5)))
+parser.add_argument('kfold', type=int, help='number of kfolds')
 
 comm = MPI.COMM_WORLD
 name = MPI.Get_processor_name()
@@ -22,35 +27,31 @@ size = comm.Get_size()
 data = load_data()
 data_oc = data_normalization(data.outcomes, 'positive')
 
-def optimize_k_error(data, mpicomm=None):
-    eps = np.arange(0.1,1.1,0.1)
-    minsamples = np.arange(5,55,5)
-    n_splits = 10
-    kf = KFold(n_splits=10)
-    indices = range(rank, n_splits, size)
-    error_mat = np.zeros((10,10,10))
-
-    for k, (train_index, test_index) in zip(indices, kf.split(data_oc)):
+def optimize_k(data, mpicomm=None):
+    kf = KFold(n_splits=args.kfold)
+    indices = range(rank, args.kfold, size)
+    error_mat = np.zeros((len(eps),len(minsamples),args.kfold))
+    k_mat = np.zeros((len(eps),len(minsamples),args.kfold))
+    for cv, (train_index, test_index) in zip(indices, kf.split(data_oc)):
         train, test = data_oc[train_index], data_oc[test_index]
         train_oc = train
         test_oc = test
-
-        for ii,e in enumerate(eps):
-
-            for jj,m in enumerate(minsamples):
+        for ii,e in enumerate(args.eps):
+            for jj,m in enumerate(args.minsamples):
                 db = DBSCAN(eps=e, min_samples=m)
                 uoinmf = UoINMF(ranks=list(range(2,20)),dbscan=db)
                 decomp = uoinmf.fit(train_oc)
                 H = decomp.components_
+                k = H.shape[0]
                 W = decomp.transform(test_oc, reconstruction_err=True)
                 error = np.linalg.norm(test_oc-W@H)
-                error_mat[ii,jj,k] = error_mat = optimize_k_error(data_oc)
-    return error_mat
+                error_mat[ii,jj,cv] = error
+                k_mat[ii,jj,cv] = k
+    return error_mat, k_mat
 
-error_mat = optimize_k_error(data_oc)
+error, k = optimize_k(data_oc)
 
 h5f = h5py.File('optimize_k_mat.h5', 'w', driver="mpio", comm=MPI.COMM_WORLD)
-h5f.create_dataset('mat', data=error_mat)
+h5f.create_dataset('error', data=error)
+h5f.create_dataset('k', data=k)
 h5f.close()
-
-plt.matshow(error_mat, cmap='gray_r')
