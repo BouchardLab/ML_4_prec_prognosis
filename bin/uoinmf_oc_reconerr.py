7021c714a8dc91f14e263e8c66f85d4d73e2a502
Ahyeon Hwang <ahyeon96@berkeley.edu>
2019-02-25 16:21:07 -0800

    script for running uoinmf oc rec err

--- bin/uoinmf_oc_reconerr.py
+++ bin/uoinmf_oc_reconerr.py
@@ -30,23 +30,23 @@ minsamples = np.arange(20,80,5)
 error_mat = np.zeros((len(eps),len(minsamples),5))
 k_mat = np.zeros((len(eps), len(minsamples), 5))
 for cv, (train_index, test_index) in enumerate(kf.split(oc_input, raw_oc[:,32])):
-    train, test = oc_input[train_index], oc_input[test_index]
-    train_oc = train
-    test_oc = test
-    for ii,e in enumerate(eps):
-        for jj,m in enumerate(minsamples):
-    	    print('eps {} minsamples {}'.format(e,m))
-            db = DBSCAN(eps=e, min_samples=m)
-            uoinmf = UoINMF(ranks=list(range(2,20)),dbscan=db)
-            decomp = uoinmf.fit(train_oc)
-            H = decomp.components_
-            W = decomp.transform(test_oc, reconstruction_err=True)
-            error = np.linalg.norm(test_oc-W@H)
-            error_mat[ii,jj,cv] = error
-            k_mat[ii,jj,cv] = H.shape[0]
+	train, test = oc_input[train_index], oc_input[test_index]
+	train_oc = train
+	test_oc = test
+	for ii,e in enumerate(eps):
+		for jj,m in enumerate(minsamples):
+			print('eps {} minsamples {}'.format(e,m))
+			db = DBSCAN(eps=e, min_samples=m)
+			uoinmf = UoINMF(ranks=list(range(2,20)),dbscan=db)
+			decomp = uoinmf.fit(train_oc)
+			H = decomp.components_
+			W = decomp.transform(test_oc, reconstruction_err=True)
+			error = np.linalg.norm(test_oc-W@H)
+			error_mat[ii,jj,cv] = error
+			k_mat[ii,jj,cv] = H.shape[0]
 
 with h5py.File('/home/ahyeon96/results/uoinmf_oc_reconerr.h5', 'a') as f:
-    f.create_dataset('error_mat', data=error_mat)
-    f.create_dataset('k_mat', data=k_mat)
+	f.create_dataset('error_mat', data=error_mat)
+	f.create_dataset('k_mat', data=k_mat)
 
 
