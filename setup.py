747a735187375a12190b5005759ef2e1bee7c056
abessiari <aessiari@lbl.gov>
2020-05-21 19:25:46 -0700

    adding pandas dependencies and remobing build tag from sonar

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'scikit-learn', 'scipy', 'ipython', 'umap-learn', 'h5py'],
+    install_requires=['numpy', 'scikit-learn', 'scipy', 'ipython', 'umap-learn', 'h5py', 'pandas'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
