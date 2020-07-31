6115e29be75af1c01589e5fc25c171c24a402234
Andrew Tritt <ajtritt@lbl.gov>
2020-07-31 15:36:46 -0700

    add shell script to run NMF and CCA

---
+++ scripts/run_all.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+function log ()
+{
+    local dir=$1
+    echo "`date` - $1"
+}
+
+ENV=${1:?"Missing conda environment"};
+INPUT=${2:?"Missing input"};
+OUTPUT=${3:?"Missing pattern to grep for"};
+LOG=$OUTPUT/$LOG
+
+conda activate $ENV
+mkdir -p $OUTDIR
+log "Copying $INPUT to $OUTDIR"
+cp $INPUT $OUTDIR
+INPUT=$OUTDIR/`basename $INPUT`
+log "Using $INPUT"
+
+log "Running UoI-NMF"
+run_uoinmf.py -b 20 -s 1001 -O 6 -B 8 $INPUT
+
+log "Plotting NMF results"
+mkdir -p $OUTDIR/nmf
+plot_uoinmf_results.py $INPUT -o $OUTPUT/nmf
+
+log "Running ALS CCA"
+run_cca.py $INPUT
+
+log "Plotting CCA results with NMF labelling"
+mkdir -p $OUTDIR/cca/nmf_labels
+plot_cca_results.py $INPUT -o $OUTPUT/cca/nmf_labels
+
+log "Plotting CCA results with clinical groupings"
+mkdir -p $OUTDIR/cca/clinical_labels
+plot_cca_results.py $INPUT -o $OUTPUT/cca/clinical_labels
