3479bb2645cd65fcc8fe16b38e274bb0581f45b1
Andrew Tritt <ajtritt@lbl.gov>
2021-02-04 17:26:15 -0800

    save uoi-alscca

--- activ/cca/alscca.py
+++ activ/cca/alscca.py
@@ -2,6 +2,7 @@ from sklearn.utils import check_random_state
 from sklearn.preprocessing import normalize
 from sklearn.linear_model import Lasso
 from sklearn.base import BaseEstimator
+from sklearn.cross_decomposition import CCA
 import numpy as np
 import numpy.linalg as LA
 from sklearn.linear_model import enet_path
@@ -16,6 +17,7 @@ def gs(A, M):
     """
     Gram-Schmidt with inner product for M
     """
+    orig_A = A
     A = A.copy()
     A[:, 0] = A[:, 0] / np.sqrt(inprod(A[:, 0], M))
     for i in range(1, A.shape[1]):
@@ -24,20 +26,27 @@ def gs(A, M):
             Aj = A[:, j]
             t = inprod(Ai, M, Aj)
             Ai = Ai - t * Aj
-        A[:, i] = Ai / np.sqrt(inprod(Ai, M))
+        norm = np.sqrt(inprod(Ai, M))
+        if norm == 0:
+            A[:, i] = Ai
+        else:
+            A[:, i] = Ai / norm
     return A
 
 
 def cdsolve(X, Y, init, reg, random_state, max_iter=1000, tol=0.1, selection='cyclic'):
     ret = np.zeros((X.shape[1], Y.shape[1]), dtype=np.float64)
     for i in range(Y.shape[1]):
-        _, this_coef, this_dual_gap = \
-            enet_path(X, Y[:, i], coef_init=init[:, i], alphas=[reg],
-                      random_state=random_state, max_iter=max_iter,
-                      selection=selection, precompute='auto',
-                      check_input=True, return_n_iters=False,
-                      tol=0.1,
-                      l1_ratio=1.0, eps=None, n_alphas=None)
+        try:
+            _, this_coef, this_dual_gap = \
+                enet_path(X, Y[:, i], coef_init=init[:, i], alphas=[reg],
+                          random_state=random_state, max_iter=max_iter,
+                          selection=selection, precompute='auto',
+                          check_input=True, return_n_iters=False,
+                          tol=tol,
+                          l1_ratio=1.0, eps=None, n_alphas=None)
+        except ValueError:
+            breakpoint()
         ret[:, i] = this_coef.squeeze()
     return ret
 
@@ -89,15 +98,17 @@ class TALSCCA(BaseEstimator):
     Alternating Least Squares Canonical Correlation Analysis
     """
 
-    def __init__(self, random_state=None,
+    def __init__(self, random_state=None, scale=False,
                  n_components=1, max_iters=100, alpha_x=0.001,
                  alpha_y=0.001):
         """
         Args:
-            X_lm:                     Linear model for regressing X onto Y
-            Y_lm:                     Linear model for regressing Y onto X
-            max_iter:                 max_iter for default Lasso object
-            alpha:                    alpha for default Lasso object
+            n_components:               the number of canonical variates to compute
+            max_iter:                   maximum number of alternating least-squares
+                                        iterations
+            alpha_x:                    regularization for regressing Y onto X
+            alpha_y:                    regularization for regressing X onto Y
+            scale:                      standardize X and Y matrices before fitting
         """
 
         self.random_state = check_random_state(random_state)
@@ -105,8 +116,16 @@ class TALSCCA(BaseEstimator):
         self.max_iters = max_iters
         self.alpha_x = alpha_x
         self.alpha_y = alpha_y
+        self.x_scale = None
+        self.y_scale = None
+        if scale:
+            self.x_scale = StandardScaler()
+            self.y_scale = StandardScaler()
 
     def fit(self, X, Y):
+        if self.x_scale is not None:
+            X = self.x_scale.fit_transform(X)
+            Y = self.x_scale.fit_transform(Y)
         ret = tals_cca(X, Y, self.n_components, max_iters=self.max_iters,
                        rx=self.alpha_x, ry=self.alpha_y, return_cov=True,
                        random_state=self.random_state)
@@ -118,8 +137,11 @@ class TALSCCA(BaseEstimator):
         return self
 
     def transform(self, X, Y):
-        if getattr(self, 'X_weights_', None) is None:
+        if getattr(self, 'x_weights_', None) is None:
             raise ValueError("TALSCCA is not fit")
+        if self.x_scale is not None:
+            X = self.x_scale.transform(X)
+            Y = self.y_scale.transform(Y)
         return X.dot(self.x_weights_), Y.dot(self.y_weights_)
 
     def fit_transform(self, X, Y):
@@ -182,25 +204,29 @@ class ALSCCA(BaseEstimator):
 
 from scipy.spatial.distance import pdist, squareform
 from scipy.cluster.hierarchy import linkage, cut_tree
-from scipy.stats import pearsonr
+import scipy.stats as sps
+from sklearn.base import BaseEstimator
+from sklearn.cross_decomposition import CCA
+from sklearn.model_selection import ParameterGrid
 
 class UoI_sCCA_Base(BaseEstimator):
 
 
-    def __init__(self, n_components=2, metric='euclidean', cons_meth='median', n_boots=20,
-                       subsample_fraction=0.9, cm_kwargs=None, random_state=None, score_function='bic'):
-        if cons_meth == 'median':
-            self.cons_meth = np.median
-            self.cm_kwargs = {'axis': 1}
-        elif cons_meth == 'mean':
-            self.cons_meth = np.mean
-            self.cm_kwargs = {'axis': 1}
+    def __init__(self, n_components=2, metric='jaccard', binary_cluster=True, n_boots=20, intersect_fraction=1.0,
+                       subsample_fraction=0.9, random_state=None, score_function='bic'):
+        """
+        Args:
+            binary_cluster (bool)           : cluster samples based on absence/presence of weights. default=True
+            metric (string)                 : the distance metric to use. If *binary_cluster* is True, use 'jaccard'.
+                                              If *binary_cluster* is False, use 'euclidean'
+        """
         self.random_state = check_random_state(random_state)
         self.n_components = n_components
-        self.metric = metric
         self.n_boots = n_boots
         self.subsample_fraction = subsample_fraction
-        self.est_cca = CCA(n_components=1, scale=False, )
+        self.est_cca = CCA(n_components=1, scale=False)
+        self.sel_cca = TALSCCA()
+        self.intersect_fraction = intersect_fraction
         if score_function == 'bic':
             self.score_function = self.bic
         elif score_function == 'pearsonr':
@@ -208,6 +234,30 @@ class UoI_sCCA_Base(BaseEstimator):
         else:
             msg = "Unrecognized score_function: '%s' -- please choose from 'bic' or 'pearsonr'" % score_function
             raise ValueError(msg)
+        self.binary_cluster = binary_cluster
+        self.metric = metric
+        if metric is None:
+            if self.binary_cluster:
+                self.metric = 'jaccard'
+            else:
+                self.metric = 'euclidean'
+
+        self.params = ParameterGrid({'alpha_x': np.logspace(-5, -1, 5),
+                                     'alpha_y': np.logspace(-5, -1, 5),
+                                     'n_components': [n_components],
+                                     'random_state': [self.random_state],
+                                     'max_iters': [1000]})
+
+        self.params = ParameterGrid({'alpha_x': np.logspace(-2, -1, 2),
+                                     'alpha_y': np.logspace(-2, -1, 2),
+                                     'n_components': [n_components],
+                                     'random_state': [self.random_state],
+                                     'max_iters': [1000]})
+
+
+    @classmethod
+    def intersect(cls, samples, axis=0, perc=1.0):
+        return (np.mean(samples, axis=axis) > perc).astype(int)
 
     @classmethod
     def ll(cls, x, y):
@@ -222,10 +272,14 @@ class UoI_sCCA_Base(BaseEstimator):
 
     @classmethod
     def bic(cls, x, y, parameters):
+        """
+        Return the negative BIC. Negate the BIC to be consistent
+        with other scoring measures.
+        """
         k = np.sum(parameters != 0)
         L = cls.ll(x, y)
         n = len(x)
-        return k * np.log(n) - 2 * L
+        return  2 * L - k * np.log(n)
 
     def fit(self, X, Y):
         n = X.shape[0]
@@ -234,7 +288,7 @@ class UoI_sCCA_Base(BaseEstimator):
         k = self.n_components
         n_params = len(self.params)
 
-        loading_samples = np.zeros(len(self.params), self.n_boots, k, (p + q))
+        weights_samples = np.zeros((len(self.params), self.n_boots, k, (p + q)))
 
         idx = np.arange(n)
         boot_samples = np.array([self.random_state.permutation(n) for i in range(self.n_boots)])
@@ -243,23 +297,27 @@ class UoI_sCCA_Base(BaseEstimator):
         #################
         # BEGIN selection
         #################
+        print("selection")
         for param_j in range(n_params):
             for boot_i in range(self.n_boots):
                 idx = boot_samples[boot_i, :rep_idx]
                 X_rep, Y_rep = X[idx], Y[idx]
-                self.sparse_cca.set_params(**self.params[param_j])
-                self.sparse_cca.fit(X_rep, Y_rep)
-                loading_samples[param_j, boot_i, :, :p] = self.sparse_cca.X_weights_
-                loading_samples[param_j, boot_i, :, p:] = self.sparse_cca.Y_weights_
+                print(self.params[param_j])
+                self.sel_cca.set_params(**self.params[param_j])
+                self.sel_cca.fit(X_rep, Y_rep)
+                weights_samples[param_j, boot_i, :, :p] = self.sel_cca.x_weights_.T
+                weights_samples[param_j, boot_i, :, p:] = self.sel_cca.y_weights_.T
 
-        loading_samples = loading_samples.reshape(len(self.params), self.n_boots*k, p + q)
-        models = np.zeros((n_params, k, p + q))
+        weights_samples = weights_samples.reshape(len(self.params), self.n_boots*k, p + q)
+        models = np.zeros((n_params, k, p + q), dtype=bool)
         for param_j in range(n_params):
             # Cluster occupancy vectors
-            samples = (loading_samples[param_j] != 0).astype(int)
-            labels = cut_tree(linkage(dist(samples, metric=self.metric), method='ward'), n_clusters=k)[:,0]
+            samples = (weights_samples[param_j] != 0).astype(int)
+            labels = cut_tree(linkage(pdist(samples, metric=self.metric), method='ward'), n_clusters=k)[:,0]   # This is the intersecting operation
             for label in np.unique(labels):   # labels <=> components
-                models[param_j, label, :] = self.cons_meth(samples[labels==label], **self.cm_kwargs) != 0
+                models[param_j, label, :] = np.ceil(np.median(samples[labels==label], axis=0))
+
+        self.weight_samples_ = weights_samples
         ###############
         # END selection
         ###############
@@ -267,33 +325,42 @@ class UoI_sCCA_Base(BaseEstimator):
         ##################
         # BEGIN estimation
         ##################
-        scores = np.zeros(n_params)
+        print("estimation")
+        scores = np.zeros((self.n_boots, n_params))
+        estimates = np.zeros((self.n_boots, n_params, k*(p + q)))
         for model_i in range(n_params):
             for boot_i in range(self.n_boots):
                 train_idx = boot_samples[boot_i, :rep_idx]
                 test_idx = boot_samples[boot_i, rep_idx:]
-                X_weight = np.zeros(k, p)
-                Y_weight = np.zeros(k, q)
-                X_pred = np.zeros(k*n)
-                Y_pred = np.zeros(k*n)
+                X_weight = np.zeros((k, p))
+                Y_weight = np.zeros((k, q))
+                X_pred = np.zeros((k*n, 1))
+                Y_pred = np.zeros((k*n, 1))
                 s = 0
                 e = 0
                 for component_i in range(k):
-                    e += k
                     model = models[model_i, component_i]
                     X_mask, Y_mask = np.array_split(model, [p])
-                    X_train = X[train_idx, X_mask]
-                    Y_train = Y[train_idx, Y_mask]
-                    X_test = X[test_idx, X_mask]
-                    Y_test = Y[test_idx, Y_mask]
+                    if np.logical_not(X_mask).all() or np.logical_not(Y_mask).all():
+                        X_weight[component_i, X_mask] = 0
+                        Y_weight[component_i, Y_mask] = 0
+                        continue
+                    X_train = X[train_idx][:, X_mask]
+                    Y_train = Y[train_idx][:, Y_mask]
+                    X_test = X[test_idx][:, X_mask]
+                    Y_test = Y[test_idx][:, Y_mask]
                     self.est_cca.fit(X_train, Y_train)
-                    X_weight[component_i, X_mask] = self.est_cca.x_weights_
-                    Y_weight[component_i, Y_mask] = self.est_cca.y_weights_
-                    X_pred[s:e], Y_pred[s:e] = self.est_cca.transform(X_test, Y_test)
+                    X_weight[component_i, X_mask] = self.est_cca.x_weights_.flatten()
+                    Y_weight[component_i, Y_mask] = self.est_cca.y_weights_.flatten()
+                    e += X_test.shape[0]
+                    _x, _y = self.est_cca.transform(X_test, Y_test)
+                    if len(_x.shape) == 1: _x = _x.reshape(-1, 1)
+                    if len(_y.shape) == 1: _y = _y.reshape(-1, 1)
+                    X_pred[s:e], Y_pred[s:e] = _x, _y # self.est_cca.transform(X_test, Y_test)
                     s = e
                 estimates[boot_i, model_i, :k*p] = X_weight.flatten()
                 estimates[boot_i, model_i, k*p:] = Y_weight.flatten()
-                scores[boot_i, model_i] = self.score_function(X_pred, Y_pred, estimates[boot_i, model_i])
+                scores[boot_i, model_i] = self.score_function(X_pred.flatten(), Y_pred.flatten(), estimates[boot_i, model_i])
 
         # calculate the best model for each bootstrap
         model_max_idx = np.argmax(scores, axis=1)
@@ -311,6 +378,6 @@ class UoI_sCCA_Base(BaseEstimator):
         return self
 
     def transform(self, X, Y):
-        if getattr(self, 'X_weights_', None) is None:
+        if getattr(self, 'x_weights_', None) is None:
             raise ValueError("TALSCCA is not fit")
         return X.dot(self.x_weights_), Y.dot(self.y_weights_)
